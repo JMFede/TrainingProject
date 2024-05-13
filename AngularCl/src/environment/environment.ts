dc1dc8568687e3d27471ed31ffc63898003ec8c3
JMFede <federico.moscato22@gmail.com>
2024-05-13 16:47:33 +0200

    kubedp

--- AngularCl/src/environment/environment.ts
+++ AngularCl/src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: "http://backend:8080/"//window.location.origin + "/"//"http://localhost:8080/"
+    API_URL: "http://10.0.17.51:8080/"//window.location.origin + "/"//"http://localhost:8080/"
 };
